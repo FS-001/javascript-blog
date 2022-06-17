5f72fc7169dd1fa77433d1405cbe51183a7d689a
Jarek Doliński <jaroslaw.dolinski@hotmail.com>
2022-06-17 09:30:05 +0200

    Generate HTML of tagWrappers

--- js/script.js
+++ js/script.js
@@ -143,10 +143,11 @@
       for (let tag of articleTagsArray) {
         /* generate HTML of the link */
         const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
-        console.log('linkHTML', linkHTML);
+        //console.log('linkHTML', linkHTML);
 
         /* add generated code to html variable */
-
+        html = html + linkHTML;
+        console.log('html', html);
         /* END LOOP: for each tag */
       }
       /* insert HTML of all the links into the tags wrapper */
